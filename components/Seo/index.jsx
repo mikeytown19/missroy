3e2b9d7b2b91173ec8f045b8647b8435f83dbee8
Michael Harvey <mikeytown19@gmail.com>
2022-04-23 07:16:37 -0600

    added new seo and image for miss roy

--- components/Seo/index.jsx
+++ components/Seo/index.jsx
@@ -4,23 +4,23 @@ import Head from 'next/head'
 const Seo = ({ page = '' }) => {
   return (
     <Head>
-      <title>Riverview Ranch | {page ? page : 'Home'}</title>
-      <title>Riverview Ranch | Home</title>
-      <meta name="description" content="Welcome to Riverview ranch, a beautiful place right here in Ogden, Utah. Come see us and see it for yourself." />
+      <title>Miss Roy | {page ? page : 'Home'}</title>
+      <title>Miss Roy | Home</title>
+      <meta name="description" content="Miss Roy" />
 
 
       <meta property="og:url" content="https://60b9-24-2-105-71.ngrok.io/" />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content="Riverview Ranch | Home" />
-      <meta property="og:description" content="Welcome to Riverview ranch, a beautiful place right here in Ogden, Utah. Come see us and see it for yourself." />
+      <meta property="og:title" content="Miss Roy | Home" />
+      <meta property="og:description" content="Welcome to Miss Roy, a beautiful place right here in Ogden, Utah. Come see us and see it for yourself." />
       <meta property="og:image" content="/preview_card.png" />
 
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:domain" content="60b9-24-2-105-71.ngrok.io" />
       <meta property="twitter:url" content="https://60b9-24-2-105-71.ngrok.io/" />
-      <meta name="twitter:title" content="Riverview Ranch | Home" />
-      <meta name="twitter:description" content="Welcome to Riverview ranch, a beautiful place right here in Ogden, Utah. Come see us and see it for yourself." />
+      <meta name="twitter:title" content="Miss Roy | Home" />
+      <meta name="twitter:description" content="Welcome to Miss Roy, a beautiful place right here in Ogden, Utah. Come see us and see it for yourself." />
       <meta name="twitter:image" content="/preview_card.png" />
 
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
