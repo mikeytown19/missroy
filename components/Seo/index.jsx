7cf3b7d8786eab6153a461b776e9086bfc9b88b4
Michael Harvey <Mikeytown19@gmail.com>
2024-05-17 13:12:38 -0600

    changing seo card

--- components/Seo/index.jsx
+++ components/Seo/index.jsx
@@ -9,19 +9,19 @@ const Seo = ({ page = '' }) => {
       <meta name="description" content="Miss Roy" />
 
 
-      <meta property="og:url" content="https://60b9-24-2-105-71.ngrok.io/" />
+      <meta property="og:url" content="https://www.missroy.org/" />
       <meta property="og:type" content="website" />
       <meta property="og:title" content="Miss Roy | Home" />
       <meta property="og:description" content="Welcome to Miss Roy, a beautiful place right here in Ogden, Utah. Come see us and see it for yourself." />
-      <meta property="og:image" content="/preview_card.png" />
+      <meta property="og:image" content="/preview_card2024.png" />
 
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:domain" content="60b9-24-2-105-71.ngrok.io" />
-      <meta property="twitter:url" content="https://60b9-24-2-105-71.ngrok.io/" />
+      <meta property="twitter:url" content="https://www.missroy.org/" />
       <meta name="twitter:title" content="Miss Roy | Home" />
       <meta name="twitter:description" content="Welcome to Miss Roy, a beautiful place right here in Ogden, Utah. Come see us and see it for yourself." />
-      <meta name="twitter:image" content="/preview_card.png" />
+      <meta name="twitter:image" content="/preview_card2024.png" />
 
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="shortcut icon" href="/favicon.ico" />
